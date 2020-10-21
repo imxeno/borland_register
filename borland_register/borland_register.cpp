a29d1f0af5d239c476d2c389ba74e982f6990b59
Piotr Adamczyk <xeno@xeno.yt>
2020-10-21 23:35:48 +0200

    initial commit

---
+++ borland_register/borland_register.cpp
@@ -0,0 +1,101 @@
+﻿// borland_register.cpp : Defines the entry point for the application.
+//
+
+#include "borland_register.h"
+
+using namespace asmjit;
+using namespace x86;
+
+void* borland_register::translator::cdecl_to_register(void* func, int argc)
+{
+	CodeHolder code;
+	code.init(runtime_.environment());
+	Assembler a(&code);
+	
+	auto offset = 0;
+	for(auto i = 3; i < argc; i++)
+	{
+		a.push(ptr(esp, 16 + offset * 2, 4));
+		
+		offset += 4;
+	}
+
+	if(argc > 2)
+	{
+		a.mov(ecx, ptr(esp, 12 + offset, 4));
+	}
+
+	if (argc > 1)
+	{
+		a.mov(edx, ptr(esp, 8 + offset, 4));
+	}
+
+	if (argc > 0)
+	{
+		a.mov(eax, ptr(esp, 4 + offset, 4));
+	}
+
+	a.call(func);
+	a.ret();
+
+	void* proxy_address;
+	Error err = runtime_.add(&proxy_address, &code);
+	
+	if(err)
+	{
+		return nullptr;
+	}
+
+	return proxy_address;
+}
+
+void* borland_register::translator::register_to_cdecl(void* func, int argc)
+{
+	CodeHolder code;
+	code.init(runtime_.environment());
+	Assembler a(&code);
+
+	for (auto i = 3; i < argc; i++)
+	{
+		const auto offset = ((i - 3) * 8) + 4;
+		a.push(ptr(esp, offset, 4));
+	}
+
+	if (argc > 2)
+	{
+		a.push(ecx);
+	}
+
+	if (argc > 1)
+	{
+		a.push(edx);
+	}
+
+	if (argc > 0)
+	{
+		a.push(eax);
+	}
+
+	
+	a.call(func);
+	a.add(esp, argc * 4);
+
+	// Remove parameters from stack but preserve return address and EAX
+	a.push(eax);
+	a.mov(eax, ptr(esp, 4));
+	a.mov(ptr(esp, (argc - 2) * 4), eax);
+	a.pop(eax);
+	a.add(esp, (argc - 3) * 4);
+
+	a.ret();
+
+	void* proxy_address;
+	Error err = runtime_.add(&proxy_address, &code);
+	
+	if (err)
+	{
+		return nullptr;
+	}
+
+	return proxy_address;
+}
---
+++ borland_register/borland_register.h
@@ -0,0 +1,20 @@
+﻿// borland_register.h : Include file for standard system include files,
+// or project specific include files.
+
+#ifndef BORLAND_REGISTER_H
+#define BORLAND_REGISTER_H
+
+#include "asmjit/asmjit.h"
+
+namespace borland_register
+{
+	class translator {
+	public:
+		void* cdecl_to_register(void* func, int argc);
+		void* register_to_cdecl(void* func, int argc);
+	private:
+		asmjit::JitRuntime runtime_;
+	};
+}
+
+#endif
